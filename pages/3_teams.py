e5c5206226d556c9d6e62f3679c800a8abe7e515
roger <roger@policiacivil.pe.gov.br>
2024-06-04 17:50:10 -0300

    ajustes

--- pages/3_teams.py
+++ pages/3_teams.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Teams",
+    page_icon="⚽",
+    layout="wide"
+)
+
 df_data = st.session_state["data"]
+
+clubes = df_data["Club"].value_counts().index
+club = st.sidebar.selectbox("Clube", clubes)
+
+df_filtered = df_data[df_data["Club"] == club].set_index("Name")
+
+st.image(df_filtered.iloc[0]["Club Logo"])
+st.markdown(f"## {club}")
+
+columns = ["Age", "Photo", "Flag", "Overall", "Value", "Wage", "Height", "Weight"]
+
+st.dataframe(df_filtered[columns])
